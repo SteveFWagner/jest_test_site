882fd7b17d4ff7fffd3a834e85e949284c8084f5
Steve <steve.wagner@mx.com>
2019-10-15 16:33:49 -0600

    table rows and css

--- src/data.js
+++ src/data.js
@@ -5,6 +5,17 @@ module.exports = {
       description:'Costa Vida',
       amount:'($15.75)'
     },
-    
+    { 
+      date:'09/02/2018',
+      description:'Costco',
+      amount:'($500.50)'
+    },
+    { 
+      date:'09/03/2018',
+      description:'Payday',
+      amount:'$1500.22'
+    },
+
+
   ]
 }
