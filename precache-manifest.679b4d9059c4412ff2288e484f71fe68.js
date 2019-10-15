b02971e7481c58328677df2018a4044f986d6cb4
Steve <steve.wagner@mx.com>
2019-10-15 11:23:30 -0600

    Updates

---
+++ precache-manifest.679b4d9059c4412ff2288e484f71fe68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2298b8b88e5ae2d8e761fde66c16adc",
+    "url": "/jest_test_site/index.html"
+  },
+  {
+    "revision": "30e58ddc9872bb457604",
+    "url": "/jest_test_site/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "311c52e42ce19db595f3",
+    "url": "/jest_test_site/static/js/2.a51088d9.chunk.js"
+  },
+  {
+    "revision": "30e58ddc9872bb457604",
+    "url": "/jest_test_site/static/js/main.fa342be2.chunk.js"
+  },
+  {
+    "revision": "2ed032d2d740cb32363d",
+    "url": "/jest_test_site/static/js/runtime-main.75e1f2b3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/jest_test_site/static/media/logo.25bf045c.svg"
+  }
+]);
