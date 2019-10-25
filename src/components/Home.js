4cba1cbceea7d0409f24253e02b14eefc8743ea5
Steve <steve.wagner@mx.com>
2019-10-25 14:58:48 -0600

    firstSelectorOnPage

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,26 +4,37 @@ class Home extends Component{
   constructor(){
     super()
     this.state = {
-      content : <h3 id="loading">Loading...</h3>
+      content : <h3 id="loading">Loading...</h3>,
+      otherContent : []
     }
   }
   
-  componentDidMount(){
+  async componentDidMount(){
     setTimeout(
       () => {
         this.setState({
-          content:<h3>This is the Home page.</h3>
+          content:<h3 id="home-page">This is the Home page.</h3>
         })
       },
       2000
+    )
+    setTimeout(
+      () => {
+        this.setState({
+          otherContent:<h4 id="other-content">This is some other content.</h4>
+        })
+      },
+      2200
     )
   }
 
   render() {
     let content = this.state.content
+    let otherContent = this.state.otherContent
     return(
       <div id="home">
         {content}
+        {otherContent}
       </div>
     )
   }
