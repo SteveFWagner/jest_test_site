155cdd65b300d31bd3bdc1ee8bc3e72b5c1523e0
Steve <steve.wagner@mx.com>
2019-10-30 10:51:45 -0600

    modal

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,13 +1,6 @@
 import React, {Component} from 'react'
 
 export default class Modal extends Component{
-  constructor(props){
-    super(props)
-    this.state = {
-
-    }
-  }
-
   render() {
     console.log('props',this.props)
     return(
