4b80a087148a77b1eda18d2a77219861371ee627
Steve <steve.wagner@mx.com>
2019-10-30 14:50:18 -0600

    second modal

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -2,13 +2,16 @@ import React, {Component} from 'react'
 
 export default class Modal extends Component{
   render() {
+    const containerId = `${this.props.modal}-container`
+    const titleId = `${this.props.modal}-title`
+    const buttonId = `${this.props.modal}-button`
     return(
-      <div id="modal-container">
-        <div id="modal-title">
+      <div id={containerId}>
+        <div id={titleId}>
           <h3>I'm a super annoying Popup!</h3>
         </div>
         <p>Click here to be redirected to some non-sense.</p>
-        <button id="modal-button" onClick={this.props.closeModal}>No Thanks!</button>
+        <button id={buttonId} onClick={() => this.props.closeModal(this.props.modal, false)}>No Thanks!</button>
       </div>
     )
   }
